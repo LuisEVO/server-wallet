4790aa9142a6217b82add691135528f882b439cf
Luis Vilcarromero <luis.vilcarromero.ortiz@gmail.com>
2023-11-23 18:51:05 -0500

    adding cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { ValidationPipe, VersioningType } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
   app.enableVersioning({ type: VersioningType.URI });
   app.useGlobalFilters(new PrismaClientExceptionFilter());
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
